86f9666562d84e1abf22210006b2642284eb1f0d
Sebastian <kozlowskisebastian.pl@outlook.com>
2025-04-09 09:34:15 +0200

    Delete JS/PRZYBORNIK.js

--- JS/PRZYBORNIK.js
+++
@@ -1,7 +0,0 @@
-        document.querySelectorAll('.KOZLOWSKISEBASTIAN_PRZYBORNIK-PRZYCISK').forEach(btn => {
-            btn.onclick = () => {
-                const polish = {'Ą':'A','Ć':'C','Ę':'E','Ł':'L','Ń':'N','Ó':'O','Ś':'S','Ź':'Z','Ż':'Z','ą':'a','ć':'c','ę':'e','ł':'l','ń':'n','ó':'o','ś':'s','ź':'z','ż':'z'};
-                const path = btn.innerText.toUpperCase().replace(/[ĄĆĘŁŃÓŚŹŻąćęłńóśźż]/g, m => polish[m]).replace(/ /g, '-');
-            location.href = `https://kozlowskisebastian.pl/${path}/`;
-            };
-        });
