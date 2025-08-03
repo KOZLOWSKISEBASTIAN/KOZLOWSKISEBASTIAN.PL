d228087a7320a4216790e3eb2a207da76f078270
SEBASTIAN <kozlowskisebastian.pl@outlook.com>
2025-08-03 21:17:45 +0200

    Update and rename bazowy.js to ROOT.js

---
+++ JS/ROOT.js
@@ -0,0 +1,11 @@
+import { ustawMotywNaPodstawieGodziny, initThemeSwitcher } from 'https://kozlowskisebastian.pl/JS/KOLORYSTYKA.js';
+import initHexagon from 'https://kozlowskisebastian.pl/JS/HEXAGON.js';
+import initButtons from 'https://kozlowskisebastian.pl/JS/PRZYCISKI.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    ustawMotywNaPodstawieGodziny();
+    initThemeSwitcher();
+    initHexagon();
+    initButtons();
+
+});
