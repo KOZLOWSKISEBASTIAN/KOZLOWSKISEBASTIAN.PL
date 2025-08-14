a7f40ddfbd2262c367030fac18dfbc2fa3944bb0
SEBASTIAN <kozlowskisebastian.pl@outlook.com>
2025-08-14 15:13:50 +0200

    Update MAIN.js

--- JS/MAIN.js
+++ JS/MAIN.js
@@ -1,7 +1,7 @@
 [
-  "JS/HEXAGON.js",
-  "JS/MOTYWAUTO.js",
-  "JS/ODSYLACZE.js"
+  "https://kozlowskisebastian.pl/JS/HEXAGON.js",
+  "https://kozlowskisebastian.pl/JS/MOTYWAUTO.js",
+  "https://kozlowskisebastian.pl/JS/ODSYLACZE.js"
 ].forEach(src => {
   const script = document.createElement("script");
   script.src = src;
